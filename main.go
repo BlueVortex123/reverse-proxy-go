ce6deb07c072df9496b4c9cdbe1ec48e15156e8d
Andrei Duicu <duicu.andrei@gmail.com>
2023-04-03 00:57:23 +0300

    adding the revrse proxy logic by using atribute function Director, printing the target url path

--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Test route, %q\n", html.EscapeString(r.URL.Path))
+	targetUrl, err := url.Parse("https://jsonplaceholder.typicode.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// getting 403 Forbidden for using only NewSingleHostReverseProxy.
+	// proxy := httputil.NewSingleHostReverseProxy(targetUrl)
+
+	// switching to more complet definition
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "https",
+		Host:   "jsonplaceholder.typicode.com",
 	})
 
-	http.ListenAndServe(":8080", nil)
+	proxy.Director = func(req *http.Request) {
+		if req.Method == "GET" {
+			req.URL.Scheme = targetUrl.Scheme // for targe scheme error handling
+			req.URL.Host = targetUrl.Host     // for  http: no Host in request URL error handling
+			targetUrl.RawQuery = ""
+			req.URL.Path = "" + req.URL.Path
+			req.Host = targetUrl.Host
+
+		}
+		fmt.Println(req.URL)
+	}
+
+	http.ListenAndServe(":8080", proxy)
 
 }
