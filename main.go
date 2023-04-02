c9ac8cfbedad769475d9dbbacdeb2a22fdc2fa61
Andrei Duicu <duicu.andrei@gmail.com>
2023-04-03 00:47:00 +0300

    adding commands to DockerFile, testing listenAndServer in main.go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"html"
+	"net/http"
+)
+
 func main() {
-	// Initial commit
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Test route, %q\n", html.EscapeString(r.URL.Path))
+	})
+
+	http.ListenAndServe(":8080", nil)
+
 }
