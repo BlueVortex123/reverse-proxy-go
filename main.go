416c11690e2263b8e344c3fe633e82bec8ff63d4
Andrei Duicu <duicu.andrei@gmail.com>
2023-04-07 01:30:44 +0300

    testing modifying elements successfully for https sites

--- main.go
+++ main.go
@@ -2,51 +2,50 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 	"time"
 )
 
 func main() {
-	targetUrl, err := url.Parse("https://jsonplaceholder.typicode.com")
-	if err != nil {
-		fmt.Println(err)
+	targetUrl := &url.URL{
+		Scheme: "http",
+		Host:   "ergast.com",
 	}
 
 	// getting 403 Forbidden for using only NewSingleHostReverseProxy.
 	// proxy := httputil.NewSingleHostReverseProxy(targetUrl)
 
 	// Switching to more complex definition
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "https",
-		Host:   "jsonplaceholder.typicode.com",
-	})
+	proxy := httputil.NewSingleHostReverseProxy(targetUrl)
 
 	// director := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		// director(req)
-		if req.Method == "GET" {
-			req.URL.Scheme = targetUrl.Scheme // for targe scheme error handling
-			req.URL.Host = targetUrl.Host     // for  http: no Host in request URL error handling
-			req.Host = targetUrl.Host
+	// proxy.Director = func(req *http.Request) {
+	// 	// director(req)
+	// 	if req.Method == "GET" {
+	// 		req.URL.Scheme = targetUrl.Scheme // for targe scheme error handling
+	// 		req.URL.Host = targetUrl.Host     // for  http: no Host in request URL error handling
+	// 		req.Host = targetUrl.Host
 
-		}
-		req.Header.Add("Accept", "application/json")
-		fmt.Println(req.URL)
-	}
+	// 	}
+	// 	req.Header.Add("Accept", "application/json")
+	// 	fmt.Println(req.URL)
+	// }
 
-	proxy.Transport = &captureTransport{
-		Transport: http.DefaultTransport,
-	}
+	// proxy.Transport = &captureTransport{
+	// 	Transport: http.DefaultTransport,
+	// }
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "https://jsonplaceholder.typicode.com"+r.RequestURI, http.StatusTemporaryRedirect)
+		http.Redirect(w, r, "http://ergast.com"+r.RequestURI, http.StatusTemporaryRedirect)
 	})
+
+	proxy.Transport = &captureTransport{http.DefaultTransport}
 
 	http.ListenAndServe(":8080", proxy)
 }
@@ -55,8 +54,9 @@ type captureTransport struct {
 	Transport http.RoundTripper
 }
 
-func (ct *captureTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	resp, err := ct.Transport.RoundTrip(req)
+func (ct *captureTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	resp, err = ct.Transport.RoundTrip(req)
+	fmt.Println(resp)
 	if err != nil {
 		return nil, err
 	}
@@ -67,35 +67,16 @@ func (ct *captureTransport) RoundTrip(req *http.Request) (*http.Response, error)
 		return nil, err
 	}
 
-	resp.Body.Close()
-
-	// if resp.Header.Get("Content-Type") == "application/json; charset=utf-8" && resp.ContentLength != 0 {
-
-	var data map[string]interface{}
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		// log.Printf("error decoding: %v", err)
-		// if e, ok := err.(*json.SyntaxError); ok {
-		// 	log.Printf("syntax error of byte offset %d", e.Offset)
-		// }
-		// log.Printf("Response: %q", body)
-		return nil, err
-	}
-
-	data["title"] = "test"
-	fmt.Printf("%v\n", data)
-
-	body, err = json.Marshal(data)
+	err = resp.Body.Close()
 	if err != nil {
 		return nil, err
 	}
 
-	// ctx := req.Context()
-	// ctx = context.WithValue(ctx, "response_body", body)
-	// req = req.WithContext(ctx)
-
-	resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-	// }
+	body = bytes.Replace(body, []byte("MRData"), []byte(randomValue()), -1)
+	body_data := ioutil.NopCloser(bytes.NewReader(body))
+	resp.Body = body_data
+	resp.ContentLength = int64(len(body))
+	resp.Header.Set("Content-Length", strconv.Itoa(len(body)))
 	return resp, nil
 }
 
